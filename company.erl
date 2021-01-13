6095bd2b9efd205ab528f96c1fc596ac57c5b681
Carlo Gilmar <carlogilmar12@gmail.com>
2021-01-12 21:46:19 -0600

    Remogin stuff

--- company.erl
+++ company.erl
@@ -1,9 +1,19 @@
+-module(company).
+-export([init/0]).
+
 -include_lib("stdlib/include/qlc.hrl").
 -include("company.hrl").
 
--module(company).
--export([start/0]).
-
-start() ->
-  0
-
+init() ->
+    mnesia:create_table(employee,
+                        [{attributes, record_info(fields, employee)}]),
+    mnesia:create_table(dept,
+                        [{attributes, record_info(fields, dept)}]),
+    mnesia:create_table(project,
+                        [{attributes, record_info(fields, project)}]),
+    mnesia:create_table(manager, [{type, bag},
+                                  {attributes, record_info(fields, manager)}]),
+    mnesia:create_table(at_dep,
+                         [{attributes, record_info(fields, at_dep)}]),
+    mnesia:create_table(in_proj, [{type, bag},
+                                  {attributes, record_info(fields, in_proj)}]).
--- hrl_examples/mess_config.hrl
+++
@@ -1,6 +0,0 @@
-%%%----FILE mess_config.hrl----
-
-%%% Configure the location of the server node,
--define(server_node, messenger@super).
-
-%%%----END FILE----
