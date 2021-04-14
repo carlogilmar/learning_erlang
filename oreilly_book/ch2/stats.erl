18bef5ff6b14c4b6d9c857af7eac7e110f576a5a
Carlo Gilmar <carlogilmar12@gmail.com>
2021-04-14 00:25:16 -0500

    Adding maximum/1 and range/1

--- oreilly_book/ch2/stats.erl
+++ oreilly_book/ch2/stats.erl
@@ -1,5 +1,5 @@
 - module(stats).
-- export([minimum/1]).
+- export([minimum/1, maximum/1, range/1]).
 
 -spec(minimum(list(number())) -> number()).
 
@@ -16,3 +16,27 @@ minimum([Head|Tail], Result) ->
     true -> minimum(Tail, Head);
     false -> minimum(Tail, Result)
   end.
+
+%%% Return the max number of a given list
+
+-spec(maximum(list(number())) -> number()).
+
+maximum(NumberList) ->
+  [Result | Rest] = NumberList,
+  maximum(Rest, Result).
+
+maximum([], Result) -> Result;
+
+maximum([Head|Tail], Result) ->
+  case Head > Result of
+    true -> maximum(Tail, Head);
+    false -> maximum(Tail, Result)
+  end.
+
+%% Range
+
+-spec(range([number()]) -> [number()]).
+range(NumberList) -> [minimum(NumberList), maximum(NumberList)].
+
+
+
