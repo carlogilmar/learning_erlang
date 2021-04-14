bbb38974116ecfe561fd265e50fa0f9563b64e95
Carlo Gilmar <carlogilmar12@gmail.com>
2021-04-14 00:07:38 -0500

    Adding dates exercise

---
+++ oreilly_book/ch2/dates.erl
@@ -0,0 +1,10 @@
+-module(dates).
+-export([date_parts/1]).
+
+-spec(date_parts(list()) -> list()).
+
+date_parts(DateStr) ->
+  [YStr, MStr, DStr] = re:split(DateStr, "-", [{return, list}]),
+  [element(1, string:to_integer(YStr)),
+   element(1, string:to_integer(MStr)),
+   element(1, string:to_integer(DStr))].
