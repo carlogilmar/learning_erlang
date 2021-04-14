5542d6f860eb43938b14b69e35ac6cc60b73eb35
Carlo Gilmar <carlogilmar12@gmail.com>
2021-04-14 00:00:07 -0500

    Adding debuggers

--- oreilly_book/ch2/ask_area.erl
+++ oreilly_book/ch2/ask_area.erl
@@ -4,7 +4,9 @@
 -spec(area() -> number()).
 
 area() ->
+  io:format(" Step 1, choose your shape... \n"),
   Answer = io:get_line("R)ectangle, T)riangle, or E)llipse > "),
+  io:format(" Step 2, chart_to_shape/1 \n"),
   Shape = char_to_shape(hd(Answer)),
   case Shape of
     rectangle -> Numbers = get_dimensions("width", "height");
@@ -12,11 +14,13 @@ area() ->
     ellipse -> Numbers = get_dimensions("major axis", "minor axis");
     unknown -> Numbers = {error, "Unknown shape" ++ [hd(Answer)]}
   end,
+  io:format(" Data ready!  \n"),
   Area = calculate(Shape, element(1, Numbers), element(2, Numbers)),
   Area.
 
 -spec(char_to_shape(char()) -> atom()).
 char_to_shape(Char) ->
+  io:format(" chart_to_shape/1 Getting atom by shape selected \n"),
   case Char of
     $R -> rectangle;
     $r -> rectangle;
@@ -29,6 +33,7 @@ char_to_shape(Char) ->
 
 -spec(get_number(string()) -> number()).
 get_number(Prompt) ->
+  io:format(" get_number/1  \n"),
   Str = io:get_line("Enter "++ Prompt ++ " > "),
   {Test, _} = string:to_float(Str),
   case Test of
@@ -39,6 +44,7 @@ get_number(Prompt) ->
 
 -spec(get_dimensions(string(), string()) -> {number(), number()}).
 get_dimensions(Prompt1, Prompt2) ->
+  io:format(" get_dimensions/2  \n"),
   N1 = get_number(Prompt1),
   N2 = get_number(Prompt2),
   {N1, N2}.
